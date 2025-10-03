b35f51497d2d47d1a7dc12b0137e798015711c21
boki <lsb530@naver.com>
2025-10-03 15:24:19 +0900

    add exposed, h2, connection pool dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
+val hikaricp_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -23,6 +26,19 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    // Exposed for Java Time
+    runtimeOnly("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+
+    // DB: H2
+    implementation("com.h2database:h2:$h2_version")
+
+    // DB Config(Connection Pool)
+    implementation("com.zaxxer:HikariCP:${hikaricp_version}")
+
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
