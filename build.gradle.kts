7cad7201c51eb2f515c7fa2fe3126aa5a3ac3b6f
boki <lsb530@naver.com>
2025-10-03 16:53:37 +0900

    fix exposed-java-time runtimeOnly to implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     // Exposed for Java Time
-    runtimeOnly("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
 
     // DB: H2
     implementation("com.h2database:h2:$h2_version")
