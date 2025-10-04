9640e198cbd37bfd713847e057c8cd297455144f
boki <lsb530@naver.com>
2025-10-04 13:17:53 +0900

    add koin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
 val hikaricp_version: String by project
+val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -38,6 +39,10 @@ dependencies {
 
     // DB Config(Connection Pool)
     implementation("com.zaxxer:HikariCP:${hikaricp_version}")
+
+    // Koin
+    implementation("io.insert-koin:koin-ktor:${koin_version}")
+    implementation("io.insert-koin:koin-logger-slf4j:${koin_version}")
 
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
